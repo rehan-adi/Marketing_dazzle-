244e856666490a411bc1cb27be866209dbe8fcf9
Rehan <rehanalire52@gmail.com>
2024-05-06 01:42:54 +0530

    Add solutions component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeBanner from '../components/Home/HomeBanner'
 import HomeSecondBanner from '../components/Home/HomeSecondBanner'
+import HomeSolutions from '../components/Home/HomeSolutions'
 
 function Home() {
   return (
     <div className='bg-[#0b0c0c] text-white z-0 flex justify-start flex-col items-center max-h-fit w-full'>
         <HomeBanner />
         <HomeSecondBanner />
+        <HomeSolutions />
     </div>
   )
 }
