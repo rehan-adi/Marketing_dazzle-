87750d770d2e8814736468b6877576dbe5b23bb2
Rehan <rehanalire52@gmail.com>
2024-05-05 20:29:16 +0530

    Add Home page second banner to code base

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import HomeSecondBanner from '../components/Home/HomeSecondBanner'
 
 function Home() {
   return (
-    <div className='bg-[#0b0c0c] text-white z-0 flex justify-start items-center max-h-fit w-full'>
+    <div className='bg-[#0b0c0c] text-white z-0 flex justify-start flex-col items-center max-h-fit w-full'>
         <HomeBanner />
         <HomeSecondBanner />
     </div>
